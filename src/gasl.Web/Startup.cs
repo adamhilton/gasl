0fc09bfc3a3dd1f0615bebf6aef17050f384f2d4
Adam Hilton <adam.tyler.hilton@gmail.com>
2016-11-05 21:12:10 -0500

    Add identity services and middleware to web project

--- src/gasl.Web/Startup.cs
+++ src/gasl.Web/Startup.cs
@@ -5,16 +5,51 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using gasl.Mvc;
+using gasl.Web.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using gasl.Web.Infrastructure.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace gasl.Api
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env) 
+        {
+            CurrentEnvironment = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("conf/appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"conf/appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+            builder.AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets();
+            }
+
+            Configuration = builder.Build();    
+        }
+
+        public IConfigurationRoot Configuration { get; }
+        public IHostingEnvironment CurrentEnvironment { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<UserContext>(options =>
+                options.UseInMemoryDatabase());
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<UserContext>()
+                .AddDefaultTokenProviders();
+
             services.AddMvcWithFeatureRouting();
         }
 
@@ -27,10 +62,31 @@ namespace gasl.Api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+
+            app.UseIdentity();
+
+            app.UseCookieAuthentication(GetCookieAuthenticationConfiguration());
+
+            app.UseMvc(ConfigureRoutes);
+        }
+
+        private static CookieAuthenticationOptions GetCookieAuthenticationConfiguration()
+        {
+            return new CookieAuthenticationOptions()
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                AuthenticationScheme = "Cookie",
+                LoginPath = new PathString("/account/login"),
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true
+            };
+        }
+
+        private static void ConfigureRoutes(IRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapRoute(
+                name: "default",
+                template: "{controller=home}/{action=index}/{id?}");
         }
     }
 }
